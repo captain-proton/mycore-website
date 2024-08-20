d4a06e7bdeca4a885b117e13ea2bbb82d2204387
Eva Roddeck <eva.roddeck@uni-due.de>
2024-08-20 07:59:09 +0200

    added entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Test if the necessary volume was mounted
+if [ ! -d /public ]; then
+    echo "Volume /public does not exist. Add a volume using -v <volume name>:/public"
+    exit
+fi
+
+echo "Using repo:  '$1'"
+echo "Website url: '$2'"
+
+# Clone the git project
+git clone $1 /site
+
+# Set the working directory to the cloned project
+cd /site
+
+# Build the Java project
+mvn clean compile
+
+# Run Hugo inside the mycore.org/ subfolder
+cd /site/mycore.org
+hugo -b $2 --cleanDestinationDir
+
+# Copy built dir with volume
+cp -R /site/mycore.org/public/* /public
